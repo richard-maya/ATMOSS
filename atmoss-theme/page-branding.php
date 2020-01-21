dabc6b62d47def7e86a0921b381ccc68f417376e
Richard Maya <richard.maya94@gmail.com>
2020-01-21 16:35:03 -0600

    Atmoss Branding Page

---
+++ atmoss-theme/page-branding.php
@@ -0,0 +1,126 @@
+<?php
+/**
+* Template Name: Branding
+*/ 
+get_header(); ?>
+
+<main>
+<section class="branding-backup">
+    <div class="container">
+        <div class="row justify-content-center py-5">
+            <div class="col-12 col-md-10 col-lg-8 text-center">
+                <header>
+                    <h1>ATMOSS</h1>
+                    <h2>IDENTIDAD CORPORATIVA</h2>
+                </header>
+                <a href="http://atmoss.mx/branding/ATMOSS_ManualCorporativo.pdf" rel="nofollow" download>
+                    <button class='btn btn-atmoss' type='button'><i class="fas fa-download mr-2"></i>Descargar manual de identidad corporativa</button>
+                </a>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row justify-content-center py-5">
+            <div class="col-12 col-md-11 col-lg-10 text-center">
+                <img src="/branding/Logo/logo-principal/Atmoss_Logotipo_Color.png" alt="atmoss-logo" class="img-fluid w-50 mb-3">
+                <h3>LOGOTIPO PRIMARIO</h3>
+                <p>Cuando el logotipo se encuentra en un fondo blanco, preferentemente se debe utilizar la versión principal del logotipo.</p>
+                <a href="/branding/Logo/logo-principal/Atmoss_Logotipo.ai" class="mr-3" rel="nofollow" download>
+                    <button class='btn btn-atmoss px-4' type='button'><i class="fas fa-download mr-2"></i>.ai</button>
+                </a>
+                <a href="/branding/Logo/logo-principal/Atmoss_Logotipo.zip" class="mr-3" rel="nofollow" download>
+                    <button class='btn btn-atmoss px-4' type='button'><i class="fas fa-download mr-2"></i>.png</button>
+                </a>
+            </div>
+        </div>
+        
+        <hr>
+        
+        <div class="row justify-content-center py-5">
+            <div class="col-12 col-md-11 col-lg-10 text-center">
+                <img src="/branding/Logo/logos-alternos/Atmoss_LogosAlternos_Color.png" alt="atmoss-logo" class="img-fluid w-25 mb-3">
+                <h3>ISOTIPO</h3>
+                <p>Se puede utilizar el isotipo sólo en su forma original o en una tinta siempre y cuando se utilice alguna de los colores institucionales, blanco o negro.</p>
+                <a href="/branding/Logo/logos-alternos/Atmoss_Isotipo.ai" class="mr-3" rel="nofollow" download>
+                    <button class='btn btn-atmoss px-4' type='button'><i class="fas fa-download mr-2"></i>.ai</button>
+                </a>
+                <a href="/branding/Logo/logos-alternos/Atmoss_Isotipo.zip" class="mr-3" rel="nofollow" download>
+                    <button class='btn btn-atmoss px-4' type='button'><i class="fas fa-download mr-2"></i>.png</button>
+                </a>
+            </div>
+        </div>
+        
+        <!-- <hr>
+
+        <div class="row justify-content-center py-5">
+            <div class="col-12 col-md-11 col-lg-10 text-center">
+                <h3>LOGOTIPOS ALTERNOS</h3>
+                <p>Cuando el logotipo es usado sobre un fondo cuyo color dificulta su lectura, deberá utilizarse el logotipo en blanco o negro.</p>
+                <a href="#" class="mr-3" rel="nofollow">
+                    <button class='btn btn-atmoss px-4' type='button'><i class="fas fa-download mr-2"></i>.ai</button>
+                </a>
+                <a href="#" rel="nofollow">
+                    <button class='btn btn-atmoss px-4' type='button'><i class="fas fa-download mr-2"></i>.png</button>
+                </a>
+            </div>
+        </div> -->
+        
+        <hr>
+
+        <div class="row justify-content-center py-5">
+            <div class="col-12 col-md-11 col-lg-10 text-center">
+                <p style="font-size: 2rem;" class="mb-4"><span class="tk-atmoss mr-5 blue-text px-1 py-1" style="background-color: #BED630">UNSTEADY OVERSTEER</span><span class="tk-berthold green-text px-1 py-1" style="background-color: #2F3342">Berthold Akzidenz</span></p>
+                <h3>TIPOGRAFÍAS</h3>
+                <p>Las tipografías institucionales son <b>Unsteady Oversteer</b> (primaria) y <b>Berthold Akzidenz</b> (secundaria). La primer tipografía es utilizada en el logotipo y es decorativa, por lo que la tipografía secundaria es un complemento fácil de leer para cuerpos de texto más largos.</p>
+                <a href="/branding/Tipografias.zip" rel="nofollow" download>
+                    <button class='btn btn-atmoss' type='button'><i class="fas fa-download mr-2"></i> Descargar tipografías</button>
+                </a>
+            </div>
+        </div>
+        
+        <hr>
+
+        <div class="row justify-content-center py-5">
+            <div class="col-12 col-md-11 col-lg-10 text-center">
+                <h3 class="mb-3">PALETA DE COLORES</h3>
+                <div class="row">
+                    <div class="col-4">
+                        <i class="fas fa-palette fa-5x mb-2" style="color: #2F3342;"></i>
+                        <p>#2F3342</p>
+                    </div>
+                    <div class="col-4">
+                        <i class="fas fa-palette fa-5x mb-2" style="color: #BED630;"></i>
+                        <p>#BED630</p>
+                    </div>
+                    <div class="col-4">
+                        <i class="fas fa-palette fa-5x mb-2" style="color: #8DA222;"></i>
+                        <p>#8DA222</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row justify-content-center py-5">
+            <div class="col-12 col-md-11 col-lg-10 text-center">
+                <h3>APLICACIONES</h3>
+                <div class="row py-4">
+                    <div class="col-6">
+                        <img src="/branding/Aplicaciones/Atmoss_HojaMembretada.png" alt="atmoss-hoja-membretada" class="img-fluid w-50">
+                    </div>
+                    <div class="col-6">
+                        <img src="/branding/Aplicaciones/Atmoss_TPresentacion.jpg" alt="atmoss-tarjeta-presentacion" class="img-fluid w-75">
+                    </div>
+                </div>
+                <a href="/branding/Aplicaciones/Atmoss_Aplicaciones.zip" rel="nofollow" download>
+                    <button class='btn btn-atmoss' type='button'><i class="fas fa-download mr-2"></i> Descargar aplicaciones</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+</main>
+
+<?php get_footer(); ?>
